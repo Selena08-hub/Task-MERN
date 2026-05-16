65309a81d2341de3b37ad40bc1316b13186b24e6
student <student@uet.com>
2026-05-16 15:29:54 +0200

    lab 9

--- products-api/server.js
+++ products-api/server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const connectDB = require('./config/dbConnection');
 const { errorHandler } = require('./middleware/errorMiddleware');
-const port= process.env.PORT || 5000;
-
-const app =  express();
+connectDB();
+const app = express();
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
-
-app.use('/api/products',require('./routes/productRoutes'));
-
+app.use(express.urlencoded({ extended: false }));
+app.use('/api/products', require('./routes/productRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 app.use(errorHandler);
 
-app.listen(port,()=>console.log(`Server listening on ${port}`))
+app.listen(port, () => {
+    console.log(`Server listening on ${port}`);
+});
